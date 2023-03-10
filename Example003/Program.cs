53cc32653410675e33b762f52482359ad103c244
IgorMoskalev <moskalevmodulbank@gmail.com>
2023-03-10 23:48:10 +0700

    Добавил еще папку

---
+++ Example003/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите свое имя");
+string username = Console.ReadLine();
+
+if (username == "Игорь")
+{
+    Console.WriteLine("Здарова мужик! Ты красавчик");
+}
+else
+{
+    Console.WriteLine("Пес пошел вон!");
+}
